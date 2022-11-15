290904ba431add6cf4120c42d80ddbdd1e6453b7
ekrov <carlos.antunes.almeida@gmail.com>
2022-11-15 17:56:59 +0000

    Test

--- app/src/main/java/com/example/maps_teste/MapsActivity.java
+++ app/src/main/java/com/example/maps_teste/MapsActivity.java
@@ -34,6 +34,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     //Buenos dias
+
+
+
+    //OLa
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
